6be709c569fb73902c74924f98fa3bec37d348a1
Bhaven Patel <bhaven.s.patel-1@ou.edu>
2019-04-23 20:35:59 -0500

    TODO list

---
+++ Todo.java
@@ -0,0 +1,33 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-04-23
+OU ID_______________: 113483352
+OU EMAIL____________: bhaven.s.patel-1@ou.edu
+
+Tasks:
+1. setupRegions
+2. paintComponent
+3. mouseClicked
+4. setSelectedRegionContents
+5. evaluate
+6. setOperator.addActionListener
+7. setOperand.addActionListener
+
+Estimated Time (minutes):
+1. 10
+2. 25
+3. 5
+4. 40
+5. 10
+6. 15
+7. 20
+
+Actual Time (minutes):
+1. 7
+2. 23
+3. 10
+4. 60
+5. 15
+6. 30
+7. 75
+*/}
